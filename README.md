7963d699c044a5b203d8e3df95262d749c08277e
PCJ600 <nju_pc@163.com>
2025-03-08 02:35:08 -0500

    feat: support centos7 automatic OS-migrate

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # os_migrate
+
+基于Linux Emergency Mode定制initramfs, 实现一键式的重建分区和OS迁移(Centos 7.9 迁移到 Rocky Linux 9.5)
+
+
+
+
+
+
+
+# 导出OVF, 挂载VMDK, 备份整个根文件系统得到升级包(va.tar.xz)
---
+++ c7/deploy.sh
@@ -0,0 +1,5 @@
+mkdir -p deploy/ && rm -rf deploy/*
+cp -a resources/va.sgdisk deploy/
+cp -a output/img/initramfs-convert.img deploy/
+cp -a output/img/vmlinuz-convert deploy/
+cp -a upgrade.sh deploy/
